23db54b9f7de48bc97717b72eef3bfc49c4e184d
DonaldVN <vietplqbdaf200035@fpt.edu.vn>
2022-01-06 11:14:24 +0700

    push MVC model

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 const morgan = require("morgan");
-const { extname } = require("path");
+const route = require("./routes");
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+
+// Middle Ware
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+app.use(express.json());
+
+// XML HTTP Request, axios, fetch
 
 // HTTP Logger
 app.use(morgan("combined"));
@@ -21,13 +31,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 
-app.get("/", (req, res) => {
-	res.render("home");
-});
-app.get("/news", (req, res) => {
-	res.render("news");
-});
+// Route
+route(app);
 
+// Port
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
