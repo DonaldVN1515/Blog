4b125d567dc1bb8500a14f088e15bce605938ad0
DonaldVN <vietplqbdaf200035@fpt.edu.vn>
2022-01-06 12:51:59 +0700

    test husky again

--- src/index.js
+++ src/index.js
@@ -1,43 +1,41 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-const handlebars = require("express-handlebars");
-const morgan = require("morgan");
-const route = require("./routes");
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const route = require('./routes');
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middle Ware
-				app.use(
-	express.urlencoded({
-		extended: true,
-	})
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
 );
 
-
-
-				app.use(express.json());
+app.use(express.json());
 
 // XML HTTP Request, axios, fetch
 
 // HTTP Logger
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 
 // Template engine
 app.engine(
-	"hbs",
-	handlebars({
-		extname: ".hbs",
-	})
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources\\views"));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
 
 // Route
 route(app);
 
 // Port
 app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
